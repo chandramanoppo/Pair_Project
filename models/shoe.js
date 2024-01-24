ce2fff355fd4522cf353788b7d74306b4398c073
chandramanoppo <152083352+chandramanoppo@users.noreply.github.com>
2024-01-25 00:08:32 +0700

    Update shoe.js

--- models/shoe.js
+++ models/shoe.js
@@ -15,6 +15,8 @@ module.exports = (sequelize, DataTypes) => {
 
       Shoe.hasMany(models.Transaction)
 
+      Shoe.belongsToMany(models.Customer , {through: 'Transaction'})
+
     }
   }
   Shoe.init({
